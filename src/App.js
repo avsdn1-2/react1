f19b5b93053600c3f8bedbeb1b7b17c69ce2ff98
Andrey <vsirik20gmail.com>
2021-05-17 17:28:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 
+import data from './news.js';
+
+function formatTime(stringTime){
+    /*
+    var momentDate = moment(stringTime);
+    return momentDate.format("dd.mm.yyyy");
+    */
+}
 function App() {
+    require('./dateFormat.js');
+    let news = require('./news.json');
+    const ar = ['1','2','3'];
+    const isShow = true;
+    const color = 'yellow';
+    //console.log(news[0]);
+    console.log(formatTime(data[0].dateCreated));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+        <div>
+        {
+            data.map((el,key) =>
+                <div style={{width:'1000px',border:'1px solid green', margin:'0 auto 5px auto'}}>
+                    <div style={{width:'43%',border:'1px solid blue',float:'left'}}>
+                        {el.isSpecial == true? <div style={{color: 'red'}}>Special</div> :<div></div>}
+                        {el.link == null? <div>Заголовок: {el.title}</div> : <div>Заголовок: <a href={el.link}>{el.title}</a></div>}
+                        <div>Краткое содержание</div>
+                        <div dangerouslySetInnerHTML={{__html: el.content}} />
+                        <div>Дата: {el.dateCreated.substr(0,10)}</div>
+                        <div>Автор: {el.author}</div>
+
+                        { el.categories.length > 0 && <div>Категории</div> }
+                        {  el.categories.length > 0 &&  el.categories.map((category,i) =>
+                            <p>{category.name}</p>
+                        )
+                        }
+
+                    </div>
+
+                    { el.photo !== null && <div style={{width:'55%',float:'right'}}><img src={el.photo}/></div>}
+                    <div style={{clear:'both'}}></div>
+                </div>
+
+
+            )
+
+
+
+        }
+        </div>
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
