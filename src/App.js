d55eaebe38336b8862daec6ce2338548a4e8d064
Andrey <vsirik20gmail.com>
2021-05-28 18:29:48 +0300

    experiment_1

--- src/App.js
+++ src/App.js
@@ -10,38 +10,55 @@ function App() {
     const ar = ['1','2','3'];
     const isShow = true;
     const color = 'yellow';
-    //console.log(news[0]);
+
+
+
+
+
+    //наполняем массив data контентом - прибавляем 1000 карточек новостей
+    for (let i = 1; i < 1000; i++) {
+        const dataNext = data[0];
+        dataNext.id = Math.random().toString (36);
+        data.push(dataNext);
+    }
+
+
+
 
   return (
     <div className="App">
         <div>
         {
             data.map((el,key) =>
-                <div key={el.id} style={{width:'1000px',border:'1px solid green', margin:'0 auto 5px auto'}}>
-                    <div style={{width:'43%',border:'1px solid blue',float:'left'}}>
-                        {el.isSpecial == true? <div style={{color: 'red'}}>Special</div> :<div></div>}
-                        {el.link == null? <div>Заголовок: {el.title}</div> : <div>Заголовок: <a href={el.link}>{el.title}</a></div>}
-                        <div>Краткое содержание</div>
-                        <div dangerouslySetInnerHTML={{__html: el.content}} />
-                        <div>Дата: {el.dateCreated.substr(0,10)}</div>
-                        <div>Автор: {el.author}</div>
+            <div key={el.id + Math.random()} style={{width: '1000px', border: '1px solid green', margin: '0 auto 5px auto'}}>
+            <div style={{width: '43%', border: '1px solid blue', float: 'left'}}>
+            <div>{ key }</div>
+            {el.isSpecial == true ? <div style={{color: 'red'}}>Special</div> : <div></div>}
+            {el.link == null ? <div>Заголовок: {el.title}</div> :
+                <div>Заголовок: <a href={el.link}>{el.title}</a></div>}
+            <div>Краткое содержание</div>
+            <div dangerouslySetInnerHTML={{__html: el.content}} />
+            <div>Дата: {el.dateCreated.substr(0, 10)}</div>
+            <div>Автор: {el.author}</div>
 
-                        { el.categories.length > 0 && <div>Категории</div> }
-                        {
-                            el.categories.length > 0 &&  el.categories.map((category,i) =>
-                            <p>{category.name}</p>)
-                        }
+            {el.categories.length > 0 && <div>Категории</div>}
+            {
+                el.categories.length > 0 && el.categories.map((category, i) =>
+                    <p>{category.name}</p>)
+            }
 
-                    </div>
+            </div>
 
-                    { el.photo !== null && <div style={{width:'55%',float:'right'}}><img src={el.photo}/></div>}
-                    <div style={{clear:'both'}}></div>
-                </div>
+            {el.photo !== null && <div style={{width: '55%', float: 'right'}}><img src={el.photo}/></div>}
+            <div style={{clear: 'both'}}></div>
+            </div>
             )
+
         }
         </div>
     </div>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//выводим в консоль время начала рендеринга
+let dateSt = new Date();
+let timeStart = 'минута ' + dateSt.getMinutes() + ' ' + 'секунда ' + dateSt.getSeconds();
+console.log(timeStart);
+
+
 ReactDOM.render(<App />,document.getElementById('root')
 );
+
+//выводим в консоль время завершения рендеринга
+let dateFin = new Date();
+let timeFinish = 'минута ' + dateFin.getMinutes() + ' ' + 'секунда ' + dateFin.getSeconds();
+console.log(timeFinish);
 
 
